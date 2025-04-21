6a62fe3a05c0f28bfabd35b1dc28fdf7cbf33f77
webcrafter011 <manujchaudhari123@gmail.com>
2025-04-21 13:19:42 +0530

    exercises 3.15-3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -35,41 +35,40 @@ app.get("/api/persons", (request, response) => {
 
 // phonebook directory info
 app.get("/info", (request, response) => {
-  const date = new Date();
-  response.send(
-    `<h3>Phonebook has info for ${persons.length} people</h3>
-        ${date}
-        `
-  );
+  Contact.find({}).then((persons) => {
+    const date = new Date();
+    response.send(
+      `<h3>Phonebook has info for ${persons.length} people</h3>
+          ${date}
+          `
+    );
+  });
 });
 
 // to see single entry
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((p) => p.id === id);
-
-  if (person) {
-    response.send(person);
-  } else {
-    response.status(404).send(`person with id ${id} not found`);
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Contact.findById(request.params.id)
+    .then((contact) => {
+      if (contact) {
+        response.json(contact);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((e) => next(e));
 });
 
 // to delete an entry
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((p) => p.id === id);
-  if (person) {
-    persons = persons.filter((person) => person.id !== id);
-    response.send(person);
-    response.status(204);
-  } else {
-    response.send("person not found");
-  }
+app.delete("/api/persons/:id", (request, response, next) => {
+  Contact.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((e) => next(e));
 });
 
 // to add an entry
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   // Check if name or number is missing
@@ -79,14 +78,6 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  // Check for duplicate name
-  // const nameExists = persons.some((p) => p.name === body.name);
-  // if (nameExists) {
-  //   return response.status(400).json({
-  //     error: "Name must be unique",
-  //   });
-  // }
-
   const contact = new Contact({
     name: body.name,
     number: body.number,
@@ -95,8 +86,37 @@ app.post("/api/persons", (request, response) => {
   contact
     .save()
     .then((res) => response.json(res))
-    .catch((e) => console.log("error occured: " + e));
+    .catch((e) => next(e));
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const { number } = request.body;
+
+  Contact.findById(request.params.id).then((contact) => {
+    if (!contact) {
+      return response.status(404).end();
+    }
+
+    contact.number = number;
+
+    contact
+      .save()
+      .then((updatedContact) => response.json(updatedContact))
+      .catch((e) => next(e));
+  });
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = 3001;
 app.listen(PORT, () => {
